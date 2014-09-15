1c578b3fdf3a5ff9fd10477b71ae7ba7af2580f3
Michiel Roos <michiel@maxserv.nl>
2014-09-15 13:28:41 +0200

    [TASK] TYPO3 6.2 compatibility

---
+++ Readme.md
@@ -0,0 +1,21 @@
+Pagetree Wizard
+===============
+
+Wizard that will create a page tree for you. Feed it with a space indented tree structure of the desired pages and the pages will be created for you.
+
+I made this extension because I found the create multiple pages wizard to be too limiting. We often run into people who have a complete sitemap available up front. It would be nice if this map could be imported into the page tree at once.
+
+You can feed the wizard a space, tab or dot-indented list of titles. It will then create a page tree based on this input.
+
+You can also specify additional fields on every line. This enables you to easily fill fields like subtitle or alias.
+
+C-style comments (single and multi line) as well as empty lines are ignored.
+
+Issue tracker can be found here: [TYPO3 Forge Project Page][1]
+
+## Credits
+
+You can say thanks at [@TuurlijkNiet][5]
+
+[1]: https://forge.typo3.org/projects/extension-wizard_crpagetree "TYPO3 Forge Project Page"
+[5]: https://twitter.com/TuurlijkNiet "Twitter"
---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "wizard_crpagetree".
+ *
+ * Auto generated 15-09-2014 13:13
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Web>Func, Wizards, Create page tree',
+	'description' => 'Wizard that will create a page tree for you. Feed it with a space indented tree structure of the desired pages and the pages will be created for you.',
+	'category' => 'module',
+	'shy' => 0,
+	'version' => '1.0.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Michiel Roos',
+	'author_email' => 'extensions@donationbasedhosting.org',
+	'author_company' => 'Donation Based Hosting',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'func_wizards' => '',
+			'' => '',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
